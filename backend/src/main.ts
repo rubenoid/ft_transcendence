cae05ca066d0113c6fba6be14aa4eb186cacc4fe
Oscar Kruithof <ozziekruithof@gmail.com>
2021-12-15 18:57:20 +0100

    Feat: added swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder();
+
+  config.setTitle("Pong");
+
+  const doc = SwaggerModule.createDocument(app, config.build()) 
+  SwaggerModule.setup('api', app, doc);
   await app.listen(5000);
 }
 bootstrap();
