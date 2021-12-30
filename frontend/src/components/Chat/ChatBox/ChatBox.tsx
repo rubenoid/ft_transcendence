aabac156348c52dfa8bc7aa8b50e4bbbbcce442f
sophielouisefeith <57356744+sophielouisefeith@users.noreply.github.com>
2021-12-30 08:32:38 +0100

    sockets

--- frontend/src/components/Chat/ChatBox/ChatBox.tsx
+++ frontend/src/components/Chat/ChatBox/ChatBox.tsx
@@ -3,6 +3,9 @@ import {TopContainer, ChatBoxContainer, InputContainer} from './ChatBoxElements'
 import { TextInput, Text, List, LongList, Item } from '../../Utils/Utils';
 import { ChatContainer } from '../ChatElements';
 
+// const { Server } = require("socket.io");
+// const io = new Server(Server);
+
 const ChatBox = () => {
     return (
         <ChatBoxContainer>
