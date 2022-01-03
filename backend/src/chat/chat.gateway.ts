c1cf3ffc485ef70521319d8f447201524e946801
sophielouisefeith <57356744+sophielouisefeith@users.noreply.github.com>
2022-01-03 20:09:22 +0100

    socket

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,19 +1,65 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+// import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 
-@WebSocketGateway({cors:true})
-export class ChatGateway {
-  // message to server/ client 
-  //@SubscribeMessage('message')
-  // handleMessage(client: any, payload: any): string {
-  //   return 'Hello world!';
-  // }
-  @WebSocketServer()
-  server;
-  @SubscribeMessage('message')
-  handleMessage(@MessageBody() message:string): void {
-    // emit a message to everyone on the server 
-    this.server.emit('message', message);
-  }
+// import { Socket, Server } from 'socket.io';
+// import { io } from "socket.io-client";
+
+// // server-side
+// io.on("connection", (socket) => {
+//   console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+// });
+
+// @WebSocketGateway({cors:true})
+// export class ChatGateway {
+//   // message to server/ client 
+//   //@SubscribeMessage('message')
+//   // handleMessage(client: any, payload: any): string {
+//   //   return 'Hello world!';
+//   // }
+//   @WebSocketServer()
+//   server;
+//   @SubscribeMessage('message')
+//   handleMessage(client: Socket, payload: any): void {
+//     // emit a message to everyone on the server 
+//     console.log(payload);
+//     this.server.emit('message', 'message');
+//   }
+// }
+
+
+   
+import {
+	SubscribeMessage,
+	WebSocketGateway,
+	OnGatewayInit,
+	WebSocketServer,
+	OnGatewayConnection,
+	OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+//import { ChatService, IncomingChatMessage } from './chat.service';
+import { Logger } from '@nestjs/common';
+// import { ChatMessage } from './chat.entity';
+
+
+@WebSocketGateway()
+export class ChatGateway
+	implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+	@WebSocketServer() server: Server;
+	constructor() {}
+
+	private logger: Logger = new Logger('ChatGateway');
+
+	afterInit(server: Server) {
+		this.logger.log('Init');
+	}
+	
+	handleDisconnect(client: Socket) {
+		this.logger.log(`Chat::Client disconnected: ${client.id}`);
+	}
+	
+	handleConnection(client: Socket, ...args: any[]) {
+		this.logger.log(`Chat::Client connected: ${client.id}`);
+	}
+
 }
-
 
