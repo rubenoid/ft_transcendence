40df5da660b76569431be830a58b42fa33995408
Harou <haroutioun24@gmail.com>
2022-01-04 16:11:49 +0100

    React Router Dom, routes: profile, register -> ok

--- frontend/src/components/Chat/ChatBox/ChatBox.tsx
+++ frontend/src/components/Chat/ChatBox/ChatBox.tsx
@@ -1,53 +1,26 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {TopContainer, ChatBoxContainer, InputContainer, SendIconContainer} from './ChatBoxElements';
 import { TextInput, Text, List, LongList, Item } from '../../Utils/Utils';
 import { ChatContainer } from '../ChatElements';
-import { handlesSubmitNewMessages} from '../../socket'
 import { AiOutlineSend as SendIcon} from 'react-icons/ai';
 
-const ChatBox: React.FunctionComponent = () => {
-    const [term, setTerm] = useState('');
-    let allMsg = ['Abc', 'def'];
-    
-    const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
-      // Preventing the page from reloading
-      event.preventDefault();
-  
-      <div id = "message"></div>
-        console.log(term);
-        
-
-        allMsg.push(term); 
-        handlesSubmitNewMessages(term);
-    
-    }
-  
-
+const ChatBox = () => {
     return (
-      <ChatBoxContainer>
-        
-        <InputContainer>
-      <div className="container">
-        <form onSubmit={submitForm}>
-          <input
-            value={term}
-            onChange={(e) => setTerm(e.target.value)}
-            type="text"
-            placeholder="Enter a term"
-            className="input"
-          />
-          <button type="submit" className="btn">Submit</button>
-        </form>
-      </div>
-      </InputContainer>
-      
-        <div>
-            {allMsg.map(msg => <h4>{msg}</h4>)}
-        </div>
-    
-      </ChatBoxContainer>
+        <ChatBoxContainer>
+            <TopContainer>
+                <Text>friend</Text>
+            </TopContainer>
+            <ChatContainer>
+
+            </ChatContainer>
+            <InputContainer>
+                <TextInput type="text" onChange={(e) => ({})}></TextInput>
+                <SendIconContainer>
+                    <SendIcon/>
+                </SendIconContainer>
+            </InputContainer>
+        </ChatBoxContainer>
     );
-  };
+}
 
-
-  export default ChatBox;
+export default ChatBox;
