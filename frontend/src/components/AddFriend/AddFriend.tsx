a4c372723dd462230cd18acfae956342215e2b6b
Harou <haroutioun24@gmail.com>
2022-01-06 13:07:26 +0100

    API update

--- frontend/src/components/AddFriend/AddFriend.tsx
+++ frontend/src/components/AddFriend/AddFriend.tsx
@@ -1,27 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { Item, TextInput ,Text, Button, WidgetContainer, TextContainer, RoundButton } from '../Utils/Utils';
-import { fetchUserByUserName, User } from '../../API/API';
+import { TextInput ,Text, Button, WidgetContainer, TextContainer } from '../Utils/Utils';
+import { fetchData, User } from '../../API/API';
 import { SearchResultContainer } from './AddFriendElements';
-import { MdPersonAdd as AddIcon } from 'react-icons/md';
 
 const AddFriend = () => {
     
-    const [user, setUser] = useState<User>();
+    const [user, setUser] = useState<User>(undefined);
     const [userName, setUserName] = useState<string>('');
 
     useEffect(() => {
         async function getUsers(): Promise<User> {
-            const user: User = await fetchUserByUserName(userName);
-            console.log('USER');
-            console.log(user);
+	        const endpoint = `/user/getByUserName/${userName}`;
+            const user: User = await fetchData(endpoint);
             if (user)
                 setUser(user);
             return user;
         }
         getUsers();
     }, [userName]);
-    console.log(user);
-    console.log('UserName->' + userName);
 
     const SearchResult = () => {
         return (
@@ -30,12 +26,8 @@ const AddFriend = () => {
                 <Button>Add</Button>
             </SearchResultContainer>
         );
-    }
+    };
 
-    const UserInfo = () => {
-
-    }
-    
     return (
         <WidgetContainer>
             <TextContainer>
