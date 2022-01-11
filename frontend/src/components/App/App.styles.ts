938d28a7bc01f112f6515b17e40d457844693fc7
Oscar Kruithof <ozziekruithof@gmail.com>
2022-01-11 18:28:52 +0100

    Feat: changed font to monospace

--- frontend/src/components/App/App.styles.ts
+++ frontend/src/components/App/App.styles.ts
@@ -11,7 +11,8 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
         margin: 0;
         padding: 0;
-        font-family: 'Encode Sans Expanded', sans-serif;
+        font-family: monospace;
+        font-size: x-large;
     }
 
     html, body {
